bd2a10d252c815e0276828c2c862d71b587943ae
stephanie durr <sdurr@matrice.io>
2018-11-24 10:01:50 +0100

    first add theme

---
+++ endymion/visites/scripts/jquery.localscroll.js
@@ -0,0 +1,95 @@
+;(function( $ ){
+	var URI = location.href.replace(/#.*/,''); // local url without hash
+
+	var $localScroll = $.localScroll = function( settings ){
+		$('body').localScroll( settings );
+	};
+	$localScroll.defaults = {
+		duration:1000, // How long to animate.
+		axis:'y', // Which of top and left should be modified.
+		event:'click', // On which event to react.
+		stop:true, // Avoid queuing animations 
+		target: window, // What to scroll (selector or element). The whole window by default.
+		reset: true // Used by $.localScroll.hash. If true, elements' scroll is resetted before actual scrolling
+	};
+
+	// If the URL contains a hash, it will scroll to the pointed element
+	$localScroll.hash = function( settings ){
+		if( location.hash ){
+			settings = $.extend( {}, $localScroll.defaults, settings );
+			settings.hash = false; // can't be true
+			
+			if( settings.reset ){
+				var d = settings.duration;
+				delete settings.duration;
+				$(settings.target).scrollTo( 0, settings );
+				settings.duration = d;
+			}
+			scroll( 0, location, settings );
+		}
+	};
+
+	$.fn.localScroll = function( settings ){
+		settings = $.extend( {}, $localScroll.defaults, settings );
+
+		return settings.lazy ?
+			// use event delegation, more links can be added later.		
+			this.bind( settings.event, function( e ){
+				// Could use closest(), but that would leave out jQuery -1.3.x
+				var a = $([e.target, e.target.parentNode]).filter(filter)[0];
+				// if a valid link was clicked
+				if( a )
+					scroll( e, a, settings ); // do scroll.
+			}) :
+			// bind concretely, to each matching link
+			this.find('a,area')
+				.filter( filter ).bind( settings.event, function(e){
+					scroll( e, this, settings );
+				}).end()
+			.end();
+
+		function filter(){// is this a link that points to an anchor and passes a possible filter ? href is checked to avoid a bug in FF.
+			return !!this.href && !!this.hash && this.href.replace(this.hash,'') == URI && (!settings.filter || $(this).is( settings.filter ));
+		};
+	};
+
+	function scroll( e, link, settings ){
+		var id = link.hash.slice(1),
+			elem = document.getElementById(id) || document.getElementsByName(id)[0];
+
+		if ( !elem )
+			return;
+
+		if( e )
+			e.preventDefault();
+
+		var $target = $( settings.target );
+
+		if( settings.lock && $target.is(':animated') ||
+			settings.onBefore && settings.onBefore.call(settings, e, elem, $target) === false ) 
+			return;
+
+		if( settings.stop )
+			$target.stop(true); // remove all its animations
+
+		if( settings.hash ){
+			var attr = elem.id == id ? 'id' : 'name',
+				$a = $('<a> </a>').attr(attr, id).css({
+					position:'absolute',
+					top: $(window).scrollTop(),
+					left: $(window).scrollLeft()
+				});
+
+			elem[attr] = '';
+			$('body').prepend($a);
+			location = link.hash;
+			$a.remove();
+			elem[attr] = id;
+		}
+			
+		$target
+			.scrollTo( elem, settings ) // do scroll
+			.trigger('notify.serialScroll',[elem]); // notify serialScroll about this change
+	};
+
+})( jQuery );
---
+++ endymion/visites/scripts/jquery.scrollTo.js
@@ -0,0 +1,145 @@
+;(function( $ ){
+	
+	var $scrollTo = $.scrollTo = function( target, duration, settings ){
+		$(window).scrollTo( target, duration, settings );
+	};
+
+	$scrollTo.defaults = {
+		axis:'xy',
+		duration: parseFloat($.fn.jquery) >= 1.3 ? 0 : 1
+	};
+	$scrollTo.window = function( scope ){
+		return $(window).scrollable();
+	};
+	$.fn.scrollable = function(){
+		return this.map(function(){
+			var elem = this,
+				isWin = !elem.nodeName || $.inArray( elem.nodeName.toLowerCase(), ['iframe','#document','html','body'] ) != -1;
+
+				if( !isWin )
+					return elem;
+
+			var doc = (elem.contentWindow || elem).document || elem.ownerDocument || elem;
+			
+			return $.browser.safari || doc.compatMode == 'BackCompat' ?
+				doc.body : 
+				doc.documentElement;
+		});
+	};
+
+	$.fn.scrollTo = function( target, duration, settings ){
+		if( typeof duration == 'object' ){
+			settings = duration;
+			duration = 0;
+		}
+		if( typeof settings == 'function' )
+			settings = { onAfter:settings };
+			
+		if( target == 'max' )
+			target = 9e9;
+			
+		settings = $.extend( {}, $scrollTo.defaults, settings );
+		// Speed is still recognized for backwards compatibility
+		duration = duration || settings.speed || settings.duration;
+		// Make sure the settings are given right
+		settings.queue = settings.queue && settings.axis.length > 1;
+		
+		if( settings.queue )
+			// Let's keep the overall duration
+			duration /= 2;
+		settings.offset = both( settings.offset );
+		settings.over = both( settings.over );
+
+		return this.scrollable().each(function(){
+			var elem = this,
+				$elem = $(elem),
+				targ = target, toff, attr = {},
+				win = $elem.is('html,body');
+
+			switch( typeof targ ){
+				// A number will pass the regex
+				case 'number':
+				case 'string':
+					if( /^([+-]=)?\d+(\.\d+)?(px)?$/.test(targ) ){
+						targ = both( targ );
+						// We are done
+						break;
+					}
+					// Relative selector, no break!
+					targ = $(targ,this);
+				case 'object':
+					// DOMElement / jQuery
+					if( targ.is || targ.style )
+						// Get the real position of the target 
+						toff = (targ = $(targ)).offset();
+			}
+			$.each( settings.axis.split(''), function( i, axis ){
+				var Pos	= axis == 'x' ? 'Left' : 'Top',
+					pos = Pos.toLowerCase(),
+					key = 'scroll' + Pos,
+					old = elem[key],
+					Dim = axis == 'x' ? 'Width' : 'Height';
+
+				if( toff ){// jQuery / DOMElement
+					attr[key] = toff[pos] + ( win ? 0 : old - $elem.offset()[pos] );
+
+					// If it's a dom element, reduce the margin
+					if( settings.margin ){
+						attr[key] -= parseInt(targ.css('margin'+Pos)) || 0;
+						attr[key] -= parseInt(targ.css('border'+Pos+'Width')) || 0;
+					}
+					
+					attr[key] += settings.offset[pos] || 0;
+					
+					if( settings.over[pos] )
+						// Scroll to a fraction of its width/height
+						attr[key] += targ[Dim.toLowerCase()]() * settings.over[pos];
+				}else
+					attr[key] = targ[pos];
+
+				// Number or 'number'
+				if( /^\d+$/.test(attr[key]) )
+					// Check the limits
+					attr[key] = attr[key] <= 0 ? 0 : Math.min( attr[key], max(Dim) );
+
+				// Queueing axes
+				if( !i && settings.queue ){
+					// Don't waste time animating, if there's no need.
+					if( old != attr[key] )
+						// Intermediate animation
+						animate( settings.onAfterFirst );
+					// Don't animate this axis again in the next iteration.
+					delete attr[key];
+				}
+			});
+
+			animate( settings.onAfter );			
+
+			function animate( callback ){
+				$elem.animate( attr, duration, settings.easing, callback && function(){
+					callback.call(this, target, settings);
+				});
+			};
+			function max( Dim ){
+				var scroll = 'scroll'+Dim;
+				
+				if( !win )
+					return elem[scroll];
+				
+				var size = 'client' + Dim,
+					html = elem.ownerDocument.documentElement,
+					body = elem.ownerDocument.body;
+
+				return Math.max( html[scroll], body[scroll] ) 
+					 - Math.min( html[size]  , body[size]   );
+					
+			};
+
+		}).end();
+	};
+
+	function both( val ){
+		return typeof val == 'object' ? val : { top:val, left:val };
+	};
+
+})( jQuery );
